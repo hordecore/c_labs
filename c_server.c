855ee2ec5a34e5211bfd342ddd808a529c9aa7f5
Strizhechenko Oleg <oleg.strizhechenko@gmail.com>
2014-10-14 04:48:37 +0000

    Теперь работает на сервере

--- c_server.c
+++ c_server.c
@@ -1,15 +1,14 @@
-/*
- * sleeps 20 seconds and terminates afterwards.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <sys/stat.h>
 #include <syslog.h>
+
+#define PORT 1488
 
 void check_pid(pid_t pid) {
 	if (pid < 0)
@@ -37,17 +36,52 @@ void skeleton_daemon(void) {
 	openlog("c_daemon", LOG_PID, LOG_DAEMON);
 }
 
-int main(void) {
-	skeleton_daemon();
+void do_listen(void) {
+	int sock, listener;
+	struct sockaddr_in addr, client_addr;
+	char buf[1024];
+	int bytes_read;
 
-	while (1) {
-		syslog(LOG_NOTICE, "daemon started.");
-		sleep(20);
-		break;
+	listener = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(listener < 0) {
+		perror("socket");
+		exit(1);
 	}
 
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+		exit(2);
+	}
+
+	listen(listener, 1);
+
+	while(1) {
+		sock = accept(listener, NULL, NULL);
+		if(sock < 0) {
+			perror("accept");
+			exit(3);
+		}
+
+		while(1) {
+			bytes_read = recv(sock, buf, 1024, 0);
+			if(bytes_read <= 0)
+				break;
+			send(sock, buf, bytes_read, 0);
+		}
+
+		close(sock);
+	}
+}
+
+int main(void) {
+	skeleton_daemon();
+	syslog(LOG_NOTICE, "daemon started.");
+	do_listen();
 	syslog(LOG_NOTICE, "daemon terminated.");
 	closelog();
-
 	return EXIT_SUCCESS;
 }
